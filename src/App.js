99a2d6fc755db3e491aad6382df7145a308c6416
xruxunuka9077 <107389310+madonarevazashvili@users.noreply.github.com>
2022-08-04 22:20:55 +0400

    start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './reset.css'
+import './App.css';
+import { Routers,Router } from 'react-router-dom';
+function App() {
+  return (
+  <Routers>
+    <Router></Router>
+  </Routers>
+    
+  )
+}
+
+export default App;
---
+++ src/Context.js
@@ -0,0 +1,2 @@
+import React from "react"
+export let Context = React.createContext()
